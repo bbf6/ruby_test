59bb6e0ee296b33f5e55ef7a8e653d86912be160
alex <alex.guajardo@inroute.com.mx>
2022-10-27 12:28:49 -0600

    Add README

--- 007.rb
+++ 007.rb
@@ -1,3 +1,3 @@
 # Escriba un algoritmo que tome del usuario una palabra que
-# represente el nombre de una variable en formato camelCase
-# y devuelva su equivalente en formato snake_case.
+# represente el nombre de una variable en formato snake_case
+# y devuelva su equivalente en formato camelCase.
---
+++ README.md
@@ -0,0 +1,5 @@
+quick start: https://www.ruby-lang.org/en/documentation/quickstart/
+
+tutorial: https://www.rubyguides.com/ruby-tutorial/
+
+docs: https://ruby-doc.org/core-3.1.2/#class-index
